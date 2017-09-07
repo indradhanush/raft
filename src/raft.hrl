70e4c6b1790491d166ef5aed53d2efd906d59c00
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2017-09-07 19:29:23 +0530

    Well defined records client_message and metadata
    Add more structural information to the record client_message. It
    consists of the client_id, the message_id and the command as sent by
    the client.
    
    Add to_reply to metadata. This will store the list of client messages
    to which the leader needs to respond.

--- src/raft.hrl
+++ src/raft.hrl
@@ -12,17 +12,24 @@
 -type log_entry() :: #log_entry{}.
 
 
+-record(client_message, {
+            client_id    :: atom(),
+            message_id   :: string(),
+            command = "" :: string()
+           }).
+
+-type client_message() :: #client_message{}.
+
+
 -record(metadata, {
-            name             :: atom(),
-            nodes = []       :: [raft_node()],
-            term = 0         :: non_neg_integer(),
-            votes = []       :: [atom()],
-            voted_for = null :: atom(),
-            leader_id = null :: atom(),
-            log = []         :: [log_entry()]
+            name                 :: atom(),
+            nodes = []           :: [raft_node()],
+            term = 0             :: non_neg_integer(),
+            votes = []           :: [atom()],
+            voted_for = null     :: atom(),
+            leader_id = null     :: atom(),
+            log = []             :: [log_entry()],
+            to_reply = []        :: [client_message()]
            }).
 
 -type metadata() :: #metadata{}.
-
-
--record(client_message, {command = "" :: string()}).
